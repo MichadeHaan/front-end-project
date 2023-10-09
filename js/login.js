ec316b38f5b4d6b571bf530f089f4c3b408418e9
Micha de Haan <Micha26052004@hotmail.com>
2023-10-09 11:16:43 +0200

    .

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "gebruiker" && password === "wachtwoord") {
+        alert("Inloggen gelukt!");
+        window.location.href = "./../index.html";
+    } else {
+        alert("Inloggen mislukt. Controleer je gebruikersnaam en wachtwoord.");
+    }
+});

8d0e4b4a6acbaa68e56e9ab5a2e6e35a80a6cc25
Micha de Haan <Micha26052004@hotmail.com>
2023-10-09 11:29:24 +0200

    .

--- js/login.js
+++ js/login.js
@@ -1,13 +1,19 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    var loginForm = document.getElementById("loginForm");
+    
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault();
 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
 
-    if (username === "gebruiker" && password === "wachtwoord") {
-        alert("Inloggen gelukt!");
-        window.location.href = "./../index.html";
-    } else {
-        alert("Inloggen mislukt. Controleer je gebruikersnaam en wachtwoord.");
+            if (username === "gebruiker" && password === "wachtwoord") {
+                alert("Inloggen gelukt!");
+                window.location.href = "./../index.html";
+            } else {
+                alert("Inloggen mislukt. Controleer je gebruikersnaam en wachtwoord.");
+            }
+        });
     }
 });

13e37023a34403e73d8802401c10ba5c83cc89f7
Micha de Haan <Micha26052004@hotmail.com>
2023-10-16 12:06:02 +0200

    .

--- php/contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $bericht = $_POST["bericht"];
-
-    echo "Bedankt voor je bericht. We zullen zo snel mogelijk contact met je opnemen.";
-}
-?>

12e6cf17e6e0e0b16fde68ddfaf03e538943d2d1
Micha de Haan <Micha26052004@hotmail.com>
2023-10-19 11:17:45 +0200

    .

--- php/jouw-server-script.php
+++ php/jouw-server-script.php
@@ -1,17 +1,46 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $bericht = $_POST["bericht"];
-    
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $to = $email;
-        $subject = "klantsurvis";
-        $message = "E-mail: $email\nBericht: $bericht";
-        mail($to, $subject, $message);
-        echo "Bedankt voor je bericht. We nemen zo snel mogelijk contact met je op.";
-    } else {
-        echo "Ongeldig e-mailadres. Voer een geldig e-mailadres in.";
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.office365.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'shadow26052004@hotmail.com';
+        $mail->Password = 'shadow052004';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $recipientEmail = $_POST['email'];
+        $mail->addAddress($recipientEmail);
+        $mail->Subject = 'E-mail is succesvol verzonden';
+        $messageToRecipient = 'Beste klant, we zullen zo snel mogelijk contact met u opnemen.';
+        $mail->isHTML(true);
+        $mail->Body = $messageToRecipient;
+
+        $mail->send();
+        
+       
+        $specificEmail = 'shadow26052004@hotmail.com';
+        $mail->clearAddresses();
+
+        $mail->addAddress($specificEmail);
+        $mail->Subject = 'over de game';
+        $messageToSpecificEmail = $_POST["bericht"];
+        $mail->isHTML(true);
+        $mail->Body = $messageToSpecificEmail;
+
+        $mail->send();
+        
+        echo 'E-mails zijn succesvol verzonden.';
+    } catch (Exception $e) {
+        echo 'Er is een fout opgetreden bij het verzenden van de e-mails: ' . $mail->ErrorInfo;
     }
 }
-
 ?>

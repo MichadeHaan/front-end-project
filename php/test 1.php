b8459f9e47efa68d6f9cb0cd6995f94047de1ee1
Micha de Haan <Micha26052004@hotmail.com>
2023-11-01 09:43:36 +0100

    .

--- php/test 1.php
+++ php/test 1.php
@@ -12,13 +12,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.office365.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'shadow26052004@hotmail.com';
-        $mail->Password = 'shadow052004';
+        $mail->Username = '.......';
+        $mail->Password = '......';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         $recipientEmail = $_POST['email'];
-        $mail->setFrom('shadow26052004@hotmail.com', 'Hobart Ankunding');
         $mail->addAddress($recipientEmail);
         $mail->Subject = 'E-mail is succesvol verzonden';
         $messageToRecipient = 'Beste klant, we zullen zo snel mogelijk contact met u opnemen.';
@@ -27,16 +26,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $mail->send();
         
-        $specificEmail = 'hobart.ankunding@ethereal.email';
-        if (!empty($_POST["bericht"])) {
-            $mail->clearAddresses();
-            $mail->addAddress($specificEmail);
-            $mail->Subject = 'Over de game';
-            $messageToSpecificEmail = $_POST["bericht"];
-            $mail->isHTML(true);
-            $mail->Body = $messageToSpecificEmail;
-            $mail->send();
-        }
+       
+        $specificEmail = '.......';
+        $mail->clearAddresses();
+
+        $mail->addAddress($specificEmail);
+        $mail->Subject = 'over de game';
+        $messageToSpecificEmail = $_POST["bericht"];
+        $mail->isHTML(true);
+        $mail->Body = $messageToSpecificEmail;
+
+        $mail->send();
         
         echo 'E-mails zijn succesvol verzonden.';
     } catch (Exception $e) {
--- php/test 2.php
+++ php/test 2.php
@@ -10,15 +10,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         $mail->isSMTP();
-        $mail->Host = 'smtp.office365.com';
+        $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'shadow26052004@hotmail.com';
-        $mail->Password = 'shadow052004';
+        $mail->Username = '.......';
+        $mail->Password = '.......';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         $recipientEmail = $_POST['email'];
-        $mail->setFrom('shadow26052004@hotmail.com', 'Hobart Ankunding');
         $mail->addAddress($recipientEmail);
         $mail->Subject = 'E-mail is succesvol verzonden';
         $messageToRecipient = 'Beste klant, we zullen zo snel mogelijk contact met u opnemen.';
@@ -26,18 +25,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Body = $messageToRecipient;
 
         $mail->send();
-        
-        $specificEmail = 'hobart.ankunding@ethereal.email';
-        if (!empty($_POST["bericht"])) {
-            $mail->clearAddresses();
-            $mail->addAddress($specificEmail);
-            $mail->Subject = 'Over de game';
-            $messageToSpecificEmail = $_POST["bericht"];
-            $mail->isHTML(true);
-            $mail->Body = $messageToSpecificEmail;
-            $mail->send();
-        }
-        
+
+        $specificEmail = '.......';
+        $mail->clearAddresses();
+
+        $mail->addAddress($specificEmail);
+        $mail->Subject = 'Over de game';
+        $messageToSpecificEmail = $_POST["bericht"];
+        $mail->isHTML(true);
+        $mail->Body = $messageToSpecificEmail;
+
+        $mail->send();
+
         echo 'E-mails zijn succesvol verzonden.';
     } catch (Exception $e) {
         echo 'Er is een fout opgetreden bij het verzenden van de e-mails: ' . $mail->ErrorInfo;
